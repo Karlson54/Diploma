516e57174acd6c66170a4c57322f20c168231bda
“Karlson54” <anruban2001@gmail.com>
2026-01-20 15:25:40 +0200

    feat: add mapper for TimeEntry Related to: #39

---
+++ TimeTracker.Core/Mappings/TimeEntryMappingProfile.cs
@@ -0,0 +1,72 @@
+using AutoMapper;
+using TimeTracker.Core.DTOs.TimeEntries;
+using TimeTracker.Data.Entities;
+
+namespace TimeTracker.Core.Mappings;
+
+public class TimeEntryMappingProfile : Profile
+{
+    public TimeEntryMappingProfile()
+    {
+        // TimeEntry -> TimeEntryDto (повний маппінг з усіма назвами)
+        CreateMap<TimeEntry, TimeEntryDto>()
+            .ForMember(dest => dest.UserName, 
+                opt => opt.MapFrom(src => src.User.Name))
+            .ForMember(dest => dest.AgencyName, 
+                opt => opt.MapFrom(src => src.Agency.Name))
+            .ForMember(dest => dest.MarketName, 
+                opt => opt.MapFrom(src => src.Market.Name))
+            .ForMember(dest => dest.ContractingAgencyName, 
+                opt => opt.MapFrom(src => src.ContractingAgency.Name))
+            .ForMember(dest => dest.ClientName, 
+                opt => opt.MapFrom(src => src.Client.Name))
+            .ForMember(dest => dest.ProjectBrandName, 
+                opt => opt.MapFrom(src => src.ProjectBrand.Name))
+            .ForMember(dest => dest.MediaName, 
+                opt => opt.MapFrom(src => src.Media.Name))
+            .ForMember(dest => dest.JobTypeName, 
+                opt => opt.MapFrom(src => src.JobType.Name));
+
+        // TimeEntry -> TimeEntryListItemDto (спрощений для списків)
+        CreateMap<TimeEntry, TimeEntryListItemDto>()
+            .ForMember(dest => dest.UserName, 
+                opt => opt.MapFrom(src => src.User.Name))
+            .ForMember(dest => dest.ClientName, 
+                opt => opt.MapFrom(src => src.Client.Name))
+            .ForMember(dest => dest.ProjectBrandName, 
+                opt => opt.MapFrom(src => src.ProjectBrand.Name));
+
+        // TimeEntry -> TimeEntryDetailDto
+        CreateMap<TimeEntry, TimeEntryDetailDto>()
+            .IncludeBase<TimeEntry, TimeEntryDto>();
+
+        // CreateTimeEntryDto -> TimeEntry
+        CreateMap<CreateTimeEntryDto, TimeEntry>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.User, opt => opt.Ignore())
+            .ForMember(dest => dest.Agency, opt => opt.Ignore())
+            .ForMember(dest => dest.Market, opt => opt.Ignore())
+            .ForMember(dest => dest.ContractingAgency, opt => opt.Ignore())
+            .ForMember(dest => dest.Client, opt => opt.Ignore())
+            .ForMember(dest => dest.ProjectBrand, opt => opt.Ignore())
+            .ForMember(dest => dest.Media, opt => opt.Ignore())
+            .ForMember(dest => dest.JobType, opt => opt.Ignore());
+
+        // UpdateTimeEntryDto -> TimeEntry
+        CreateMap<UpdateTimeEntryDto, TimeEntry>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.UserId, opt => opt.Ignore()) // UserId не змінюється при оновленні
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.User, opt => opt.Ignore())
+            .ForMember(dest => dest.Agency, opt => opt.Ignore())
+            .ForMember(dest => dest.Market, opt => opt.Ignore())
+            .ForMember(dest => dest.ContractingAgency, opt => opt.Ignore())
+            .ForMember(dest => dest.Client, opt => opt.Ignore())
+            .ForMember(dest => dest.ProjectBrand, opt => opt.Ignore())
+            .ForMember(dest => dest.Media, opt => opt.Ignore())
+            .ForMember(dest => dest.JobType, opt => opt.Ignore());
+    }
+}
