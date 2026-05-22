b7629c7e112757f7210bdaacbaf911c5c76c3dc1
“Karlson54” <anruban2001@gmail.com>
2026-05-22 18:06:21 +0300

    fix: add migration for delete country from agencies

---
+++ TimeTracker.Data/Migrations/20260522114931_RemoveAgencyCountry.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAgencyCountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "country",
+                table: "agencies");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "country",
+                table: "agencies",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "Ukraine");
+        }
+    }
+}
