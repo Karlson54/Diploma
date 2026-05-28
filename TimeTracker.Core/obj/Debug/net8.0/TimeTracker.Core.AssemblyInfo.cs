4d5f6c6c49303a809a5de289d4636dab20e839b1
“Karlson54” <anruban2001@gmail.com>
2026-05-28 16:44:10 +0300

    feat: change IP in docker-compose

--- TimeTracker.Core/obj/Debug/net8.0/TimeTracker.Core.AssemblyInfo.cs
+++ TimeTracker.Core/obj/Debug/net8.0/TimeTracker.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeTracker.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f02873666ddc45e89dde67c35aaeec98800996fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71d27803323ba03665dc8227d73e469c1219e8e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimeTracker.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimeTracker.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
